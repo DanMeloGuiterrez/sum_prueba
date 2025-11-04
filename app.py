50ee8c985f80afb23b7639c4eddf58b6440426cf
DanMeloGuiterrez <melodan89@gmail.com>
2025-11-03 23:53:35 -0500

    Chat Bot mejorado

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def get_chat_response(text):
         - nombre_alumno
         - apellido_alumno
         - codigo
-        - facultad a la que le pertenece mediante su id
+        - id_facultad
         - especialidad
         - periodo_academico
 
